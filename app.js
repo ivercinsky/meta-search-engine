b6ca4188c52a4d77e21b39fee9e011d40b748f37
Ivan <ivan.vercinsky@gdsmodellica.com>
2017-01-23 14:33:42 -0300

    good logger

--- app.js
+++ app.js
@@ -71,6 +71,59 @@ server.route([{
     }
 }]);
 
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ log: '*', response: '*' }]
+        }, {
+            module: 'good-console'
+        }, 'stdout'],
+        myFileReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ ops: '*' }]
+        }, {
+            module: 'good-squeeze',
+            name: 'SafeJson'
+        }, {
+            module: 'good-file',
+            args: ['./test/fixtures/awesome_log']
+        }],
+        myHTTPReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*' }]
+        }, {
+            module: 'good-http',
+            args: ['http://prod.logs:3000', {
+                wreck: {
+                    headers: { 'x-api-key': 12345 }
+                }
+            }]
+        }]
+    }
+};
+
+server.register({
+    register: require('good'),
+    options,
+}, (err) => {
+
+    if (err) {
+        return console.error(err);
+    }
+    server.start(() => {
+        console.info(`Server started at ${ server.info.uri }`);
+    });
+
+});
+
+
 // Start the server
 server.start((err) => {
 
