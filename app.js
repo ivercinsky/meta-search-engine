2ca46399c3ed88cdd942141609668b7a1cf1f708
Ivan Vercinsky <ivercinsky@dc.uba.ar>
2017-02-09 22:32:40 -0300

    sending response in second call

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const Selector = require("./SearchEngineSelector.js");
 const ResultsMerger = require("./ResultsMerger.js");
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request-promise');
 
 //console.log(process.argv);
-console.log("USANDO DESPEGAR_API_TOKEN", process.env.DESPEGAR_API_TOKEN, "para comunicarme con Despegar.com"); 
+console.log("USANDO DESPEGAR_API_TOKEN", process.env.DESPEGAR_API_TOKEN, "para comunicarme con Despegar.com");
 
 var app = express();
 
@@ -15,43 +16,58 @@ var jsonParser = bodyParser.json();
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(function(req, res, next) {
-  res.setHeader('Content-type', 'application/json')
-  next();
+app.use(function (req, res, next) {
+    res.setHeader('Content-type', 'application/json')
+    next();
 });
-app.get('/favicon.ico',function(request, response) {
-        response.send("hola");
-    
+app.get('/favicon.ico', function (request, response) {
+    response.send("hola");
+
 });
-app.post('/', function(request, response) {
+
+var apiaiReq = request.defaults({
+    headers: {
+        'Authorization': 'Bearer ' + process.env.APIAI_TOKEN,
+    }
+});
+app.post('/', function (request, response) {
     var req = request.body.result;
     if (req.action == "buscar_vuelos") {
         var params = req.parameters;
-        response.send({
-            speech: "Buscando vuelos...",
-            displayText: "Buscando vuelos...",
-            data: {},
-            contextOut:[],
-            source:"MetaSearchEngine"
-        })
-        Selector.search(params).then(function(response) {
+        Selector.search(params).then(function (response) {
             var search = ResultsMerger.merge(response);
             // LLAMADA A API.AI CON EL EVENTO DE MOSTRAR RESULTADOS Y ESTE DATA..
-            /**var data = {
-                speech: "Estos son los resueltados que encontre!",
-                displayText: "conexion exitosa --> " + JSON.stringify(params),
-                data: { "search": search},
-                contextOut:[],
-                source:"MetaSearchEngine"
-            }
-            return response.send(data).header('Content-type','application/json');*/
+            var options = {
+                method: 'POST',
+                uri: 'https://api.api.ai/v1/query',
+                body: {
+                    event: {
+                        name: process.env.RESULTADOS_EVENT_NAME,
+                        data: {
+                            search: search
+                        }
+                    }
+                },
+                json: true // Automatically stringifies the body to JSON
+            };
+            apiaiReq.post(options).then(function (response) {
+                console.log(response);
+            });
+            return response.send({
+                speech: "Buscando vuelos...",
+                displayText: "Buscando vuelos...",
+                data: {},
+                contextOut: [],
+                source: "MetaSearchEngine"
+            })
+
         });
     } else {
         console.log(request.body.result);
     }
-    
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port0', process.env.PORT || 3000, '!');
+    console.log('Example app listening on port0', process.env.PORT || 3000, '!');
 });
