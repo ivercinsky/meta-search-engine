43f664a78cf098701d9cd05247d4a9d2a32adbc5
Ivan Vercinsky <ivercinsky@dc.uba.ar>
2017-02-09 21:53:47 -0300

    switch a express y callback con respuesta in progress

--- app.js
+++ app.js
@@ -2,137 +2,55 @@
 
 const Selector = require("./SearchEngineSelector.js");
 const ResultsMerger = require("./ResultsMerger.js");
-const Hapi = require('hapi');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 //console.log(process.argv);
 console.log("USANDO DESPEGAR_API_TOKEN", process.env.DESPEGAR_API_TOKEN, "para comunicarme con Despegar.com"); 
 
-const server = new Hapi.Server();
+var app = express();
 
-server.connection({ 
-    host: process.env.HOST || 'localhost', 
-    port: process.env.PORT || 8000 
+var jsonParser = bodyParser.json();
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/favicon.ico',function(request, response) {
+        response.send("hola");
+    
 });
-
-
-server.register(require('inert'), function() {
-    server.route({
-        method:'GET',
-        path:'/',
-        handler: function(request, reply) {
-            reply.file("./index.html");
-        }
-    });
-});
-
-server.route([{
-    method:'GET',
-    path:'/favicon.ico',
-    handler: function(request, reply) {
-        reply("hola");
-    }
-},{
-    method: 'GET',
-    path:'/{p*}', 
-    handler: function (request, reply) {
-        //var query = JSON.parse(process.argv[2].split("=")[1]);
-        var query = request.query;
-        console.log(request.path);
-        Selector.search(query, request.path).then(function(response) {
-            response = ResultsMerger.merge(response);
-            reply(response);
-        });
-    }
-}, {
-    method:'POST',
-    path:'/vuelos',
-    handler: function(request, reply) {
-        console.log("Buscando VUELOs");
-        console.log(request);
-        var req = request.payload.result;
+app.post('/', function(request, response) {
+    console.log(request.body);
+    var req = request.body.result;
+    if (req.action == "buscar_vuelos") {
         var params = req.parameters;
-        console.log(params);
-        
+        console.log(request);
+        response.send({
+            speech: "Buscando vuelos...",
+            displayText: "Buscando vuelos...",
+            data: {},
+            contextOut:[],
+            source:"MetaSearchEngine"
+        }).header('Content-type','application/json');
         Selector.search(params).then(function(response) {
             var search = ResultsMerger.merge(response);
-            var data = {
+            // LLAMADA A API.AI CON EL EVENTO DE MOSTRAR RESULTADOS Y ESTE DATA..
+            /**var data = {
                 speech: "Estos son los resueltados que encontre!",
                 displayText: "conexion exitosa --> " + JSON.stringify(params),
                 data: { "search": search},
                 contextOut:[],
                 source:"MetaSearchEngine"
             }
-            return reply(data).header('Content-type','application/json');
+            return response.send(data).header('Content-type','application/json');*/
         });
+    } else {
+        console.log(request.body.result);
     }
-}, {
-    method: 'POST',
-    path: '/',
-    handler: function(request, reply) {
-        console.log(request);
-        console.log("ENTRO SIN ACTION");
-        return reply("NO MANDO EL ACTION").header('Content-type','application/json');
-    }
-}]);
-
-const options = {
-    ops: {
-        interval: 1000
-    },
-    reporters: {
-        myConsoleReporter: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [{ log: '*', response: '*' }]
-        }, {
-            module: 'good-console'
-        }, 'stdout'],
-        myFileReporter: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [{ ops: '*' }]
-        }, {
-            module: 'good-squeeze',
-            name: 'SafeJson'
-        }, {
-            module: 'good-file',
-            args: ['./test/fixtures/awesome_log']
-        }],
-        myHTTPReporter: [{
-            module: 'good-squeeze',
-            name: 'Squeeze',
-            args: [{ error: '*' }]
-        }, {
-            module: 'good-http',
-            args: ['http://prod.logs:3000', {
-                wreck: {
-                    headers: { 'x-api-key': 12345 }
-                }
-            }]
-        }]
-    }
-};
-
-server.register({
-    register: require('good'),
-    options,
-}, (err) => {
-
-    if (err) {
-        return console.error(err);
-    }
-    server.start(() => {
-        console.info(`Server started at ${ server.info.uri }`);
-    });
-
+    
 });
 
-
-// Start the server
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port0', process.env.PORT || 3000, '!');
 });
